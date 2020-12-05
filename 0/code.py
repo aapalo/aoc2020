cf9f25bcbd7670bdc6e746cbbdfd672aadeb07da
aapalo <aapalo@users.noreply.github.com>
2020-12-05 13:11:45 +0200

    Day 0, readme

---
+++ 0/code.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+#from collections import Counter
+#import re
+#import os
+import time
+#from collections import defaultdict
+#from collections import deque
+'''     #######     '''
+
+date =
+dev = 0 # extra prints
+part = 1 # 1,2, or 3 for both
+samp = 1 # 0 or 1
+
+'''     #######     '''
+
+
+def day(te):
+
+    return 0
+
+def day2(te):
+
+    return 0
+
+'''     #######     '''
+
+time0 = time.time()
+
+if samp == 1:
+    filename = "/sample.txt"
+else:
+    filename = "/input.txt"
+
+try:
+    with open(str(date) + filename,"r") as f:
+        t = f.readlines()
+except FileNotFoundError:
+    with open("." + filename,"r") as f:
+        t = f.readlines()
+
+t = [(x.strip().replace('  ',' ')) for x in t]
+
+if part == 1:
+    print("Part 1: ", day(t))
+elif part == 2:
+    print("Part 2: ", day2(t))
+elif part == 3:
+    #run both
+    print("Part 1: ", day(t))
+    print("Part 2: ", day2(t))
+
+tdif = time.time() - time0
+print("Elapsed time: {:.4f} s".format(tdif))
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ My solutions for [Advent of code](https://adventofcode.com/2020) programming puz
 
 ## Runtimes:
 Used time.time() to measure the time difference in seconds.
-Running CPython via terminal and Atom took the same time, so values for Atom are shown.
+Running CPython via terminal and Atom took the same time, so values for Atom are shown. Go was run with Atom's Script as well.
 
-| Program   | Atom     | pypy3   | Atom/pypy3 |
-| --------  | :------: | :-----: | :--------: |
+| Program   | Python   | Go    | Python/Go | Comment |
+| --------  | :------: | :---: | :-------: | :-----: |
